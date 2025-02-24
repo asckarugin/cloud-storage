c6a5ffb33e82f63e118cb7f32320484b440b3259
Evgenii Krasikov <krasikov.ev2@gmail.com>
2025-02-24 09:44:20 +0500

    - Create basic security configuration

--- src/main/java/by/asckarugin/config/SecurityConfig.java
+++ src/main/java/by/asckarugin/config/SecurityConfig.java
@@ -1,4 +1,58 @@
 package by.asckarugin.config;
 
+import by.asckarugin.service.CustomUserDetailsService;
+import by.asckarugin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final PasswordEncode passwordEncode;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                      req -> req.requestMatchers("/api/v1/registration", "/api/v1/login")
+                              .permitAll()
+                              .anyRequest()
+                              .authenticated()
+                ).userDetailsService(customUserDetailsService)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(STATELESS))
+                .exceptionHandling((sessionManagement) -> sessionManagement
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .logout((logout) -> logout.logoutSuccessUrl("/api/v1/logout").permitAll())
+                .build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncode.passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }

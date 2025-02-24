c9320376f8289d5bea80d8af2c8af1837f35f541
Evgenii Krasikov <krasikov.ev2@gmail.com>
2025-02-24 09:45:18 +0500

    - Create UserDetailsService for users in security

--- src/main/java/by/asckarugin/service/CustomUserDetailsService.java
+++ src/main/java/by/asckarugin/service/CustomUserDetailsService.java
@@ -1,5 +1,30 @@
 package by.asckarugin.service;
 
+import by.asckarugin.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class CustomUserDetailsService {
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(
+                String.format("User %s is not found", username)
+        ));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.name())).collect(Collectors.toList())
+        );
+    }
 }
